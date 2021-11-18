60d21d592c270d8d0fa91bb2fded99bf24e74484
histlog <safejp1@gmail.com>
2021-11-19 00:53:33 +0530

    Profile delete and update features added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,4 +144,77 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user and posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // todo - remove user posts
+    // Remove profile
+    await Profile.findOneAndRemove({
+      user: req.user.id,
+    });
+    // Remove user
+    await User.findOneAndRemove({
+      _id: req.user.id,
+    });
+    res.json({ msg: 'User removed' });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('server error');
+  }
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From Date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      // Fetch existing profile that needs update
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      // Add newest to the front
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
+  }
+);
+
 module.exports = router;

cb523b0b2b76e6472d54c59b90417a236fcea796
histlog <safejp1@gmail.com>
2022-08-03 00:31:42 +0530

    Updated form output

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -13,13 +13,22 @@ export const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log('password mismatch');
+    } else {
+      console.log(formData);
+    }
+  };
+
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
       <p className='lead'>
         <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form className='form' action='create-profile.html'>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='text'
@@ -31,7 +40,13 @@ export const Register = () => {
           />
         </div>
         <div className='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
@@ -42,6 +57,8 @@ export const Register = () => {
             type='password'
             placeholder='Password'
             name='password'
+            value={password}
+            onChange={(e) => onChange(e)}
             minLength='6'
           />
         </div>
@@ -50,6 +67,8 @@ export const Register = () => {
             type='password'
             placeholder='Confirm Password'
             name='password2'
+            value={password2}
+            onChange={(e) => onChange(e)}
             minLength='6'
           />
         </div>
